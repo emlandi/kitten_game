5dde7bbfdc6d69edf39c32d16e713c54f62f2651
Emily Landi <emily.landi@gmail.com>
2015-07-20 19:51:50 -0700

    moved pics to images file and made minor changes to style

--- js/vote-tracker.js
+++ js/vote-tracker.js
@@ -1,6 +1,6 @@
 //This is our vote tracker!!!
 
-var image = ['0.jpg', '1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg'];
+var image = ['images/0.jpg', 'images/1.jpg', 'images/2.jpg', 'images/3.jpg', 'images/4.jpg', 'images/5.jpg', 'images/6.jpg', 'images/7.jpg', 'images/8.jpg', 'images/9.jpg', 'images/10.jpg', 'images/11.jpg', 'images/12.jpg', 'images/13.jpg' ];
 
 var Tracker = function() {
   this.image = image;
