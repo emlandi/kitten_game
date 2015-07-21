20621728a5d9bfc603ba14e71fc6f941d163ce36
Emily Landi <emily.landi@gmail.com>
2015-07-21 16:47:09 -0700

    randomly adds cat pics to screen and adds votes but vote tracker is not completely accurate - sometimes an extra vote is added in

--- js/vote-tracker.js
+++ js/vote-tracker.js
@@ -1,55 +1,81 @@
-//This is our vote tracker!!!
+//image constructor
+var Image = function(path) {
+  this.path = path;
+  this.votes = 0;
+}
 
-var image = ['images/0.jpg', 'images/1.jpg', 'images/2.jpg', 'images/3.jpg', 'images/4.jpg', 'images/5.jpg', 'images/6.jpg', 'images/7.jpg', 'images/8.jpg', 'images/9.jpg', 'images/10.jpg', 'images/11.jpg', 'images/12.jpg', 'images/13.jpg' ];
+//instances of image
+var cat0 = new Image ('images/0.jpg');
+var cat1 = new Image ('images/1.jpg');
+var cat2 = new Image ('images/2.jpg');
+var cat3 = new Image('images/3.jpg');
+var cat4 = new Image ('images/4.jpg');
+var cat5 = new Image('images/5.jpg');
+var cat6 = new Image ('images/6.jpg');
+var cat7 = new Image ('images/7.jpg');
+var cat8 = new Image ('images/8.jpg');
+var cat9 = new Image ('images/9.jpg');
+var cat10 = new Image ('images/10.jpg');
+var cat11 = new Image ('images/11.jpg');
+var cat12 = new Image ('images/12.jpg');
+var cat13 = new Image ('images/13.jpg');
+
+var catArray = [cat0, cat1, cat2, cat3, cat4, cat5, cat6, cat7, cat8, cat9, cat10, cat11, cat12, cat13];
 
 var Tracker = function() {
-  this.image = image;
+  this.catArray = catArray;
+  //console.log(catArray);
 }
 
 Tracker.prototype.randomNumber = function() {
-  this.random = parseInt(Math.floor(Math.random() * (this.image.length)));
+  this.random = parseInt(Math.floor(Math.random() * (this.catArray.length)));
   return this.random;
 }
 
 Tracker.prototype.images = function() {
   this.leftImage, this.rightImage;
-  this.leftImage = this.image[this.randomNumber()];
-  this.rightImage = this.image[this.randomNumber()];
-  while (this.leftImage == this.rightImage) {
-    this.rightImage = this.image[this.randomNumber()];
+
+  this.leftImageRandom = this.randomNumber();
+  this.leftImage = this.catArray[this.leftImageRandom].path;
+
+  this.rightImageRandom = this.randomNumber();
+  this.rightImage = this.catArray[this.rightImageRandom].path;
+
+  while (this.leftImageRandom == this.rightImageRandom) {
+    this.rightImageRandom = this.randomNumber();
+    this.rightImage = this.catArray[this.rightImageRandom].path;
   }
-  return [this.leftImage, this.rightImage];
+
+  console.log(this.leftImage, this.rightImage, this.leftImageRandom, this.rightImageRandom);
+
+  return [this.leftImage, this.rightImage, this.leftImageRandom, this.rightImageRandom];
 }
 
 Tracker.prototype.display = function() {
   var left = document.getElementById('left');
   var right = document.getElementById('right');
   var imgs = this.images();
+
   left.src = imgs[0];
   right.src = imgs[1];
+
+  this.leftVote = document.getElementById('left');
+  this.leftVote.addEventListener('click', function() {
+    catArray[imgs[2]].votes += 1;
+  }, false);
+
+  this.rightVote = document.getElementById('right');
+  this.rightVote.addEventListener('click', function() {
+    catArray[imgs[3]].votes += 1;
+  }, false);
 }
 
-var test = new Tracker()
-test.display();
+var render = new Tracker()
+render.display();
 
 var nextRound = document.getElementById('button');
 nextRound.addEventListener('click', function() {
-  test.display();
+  render.display();
 }, false);
 
-// var Voting = function() {
-//   this.votes = [];
-// }
 
-// Voting.prototype.vote = function () {
-//   this.leftVote = document.getElementById('left');
-//   this.leftVote.addEventListener('click', function() {
-//     this.votes.push
-//   }
-//     )
-// }
-
-
-
-
-// Tracker **could** have 'photos' property that is an array of the photo instances, and methods such as isVoting(), declareWinner(), voting(), declaring(),  updateTally(), resetResults(), castVote(), and so on.
