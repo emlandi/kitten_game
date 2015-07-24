8ee7832dabc424aa4655adb77da8c2c3c2d6a3e3
Emily Landi <emily.landi@gmail.com>
2015-07-24 15:28:54 -0700

    completed/fixed http server and 404 page

--- index.js
+++ index.js
@@ -3,24 +3,29 @@
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+// app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-app.set('views', __dirname + '/views');
+// app.set('views', __dirname + '/views');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
+// app.get('/', function(request, response) {
+  // response.render('pages/index');
+// });
 
 app.get('/secret', function(request, response) {
+  response.status(200).send("<h1>This page is secret. Go away.</h1>");
+});
+
+app.get('/random', function(request, response) {
   response.status(200).send("<h1>WELCOME, Kitten Lover!</h1>");
 });
+
 
 app.get('/*', function(req, res) {
   res.status(404).sendFile("404.html", {root: __dirname + "/public/"});
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.listen(process.env.PORT || 5000, function() {
+  console.log('The server is running on port 5000');
 });
